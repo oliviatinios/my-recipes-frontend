700747825754df89c06f993360e5163d3ed53eee
Olivia Tinios <oliviatinios@gmail.com>
2020-09-11 13:07:47 -0400

    Add toolbar to ViewRecipeScreen

--- app/screens/ViewRecipeScreen.js
+++ app/screens/ViewRecipeScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { Icon } from "react-native-elements";
 
 import colours from "../config/colours";
 
@@ -7,20 +8,32 @@ const ViewRecipeScreen = ({ route, navigation }) => {
   const { _id, title, subtitle, description } = route.params;
 
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.subtitle}>{subtitle}</Text>
-      <View style={styles.innerContainer}>
-        <Text style={styles.header}>Description</Text>
-        <Text style={styles.description}>{description}</Text>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.container}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.subtitle}>{subtitle}</Text>
+        <View style={styles.innerContainer}>
+          <Text style={styles.header}>Description</Text>
+          <Text style={styles.description}>{description}</Text>
+        </View>
+        <View style={styles.innerContainer}>
+          <Text style={styles.header}>Ingredients</Text>
+        </View>
+        <View style={styles.innerContainer}>
+          <Text style={styles.header}>Steps</Text>
+        </View>
+      </ScrollView>
+      <View style={styles.toolbar}>
+        <Icon
+          reverse
+          name="md-trash"
+          type="ionicon"
+          color={colours.dark}
+          onPress={() => console.log("test")}
+        />
+        <Icon reverse name="md-create" type="ionicon" color={colours.dark} />
       </View>
-      <View style={styles.innerContainer}>
-        <Text style={styles.header}>Ingredients</Text>
-      </View>
-      <View style={styles.innerContainer}>
-        <Text style={styles.header}>Steps</Text>
-      </View>
-    </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -52,6 +65,17 @@ const styles = StyleSheet.create({
     fontSize: 36,
     margin: 5,
     color: colours.dark,
+  },
+  toolbar: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    backgroundColor: colours.light,
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    height: 60,
   },
 });
 
